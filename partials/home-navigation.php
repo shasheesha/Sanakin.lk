6738d8bec275c0a7c80fa08d3f4906511087659d
shasheeshajd <85898263+shasheesha@users.noreply.github.com>
2023-01-21 16:38:31 +0530

    nav-bar created
    build by shasheesha.
    the nav-bar is done. not linked. expect to do linking using JS.

--- partials/home-navigation.php
+++ partials/home-navigation.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<nav id="nav">
+        <div class="container">
+            <img src="./assets/sanakin-logo.png" id="nav-logo" alt="Sanakin.lk" srcset="">
+
+            <ul class="nav-items">
+                <li id="home-page">Home</li>
+                <li id="shop-online-page">Shop Online</li>
+                <li id="whats-new-page">What's New</li>
+                <li id="contact-page">Contact</li>
+                <li id="about-page">About Us</li>
+            </ul>
+            <div id="nav-sign">
+                <input type="button" class="btn primary" id="sign-up" value="Sign up">
+                <input type="button" class="btn secondary" id="sign-in" value="Sign in">
+            </div>
+        </div>
+        <div id="nav-logo"></div>
+</nav>
