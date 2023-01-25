02f5583e8ddfe73b52cc181acbe051cbdaa4bf77
shasheeshajd <85898263+shasheesha@users.noreply.github.com>
2023-01-25 20:25:27 +0530

    home page development
    90% done. have to finish the footer.

--- Index.php
+++ Index.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/">
     <link rel="stylesheet" href="./css/main.css">
     <title>Sanakin.LK | Trusted Shops</title>
 </head>
@@ -272,24 +274,24 @@
             <h2 class="section-header">Today Best Deals For You</h2>
 
             <div class="row products">
-                
-            <ul class="nav nav-pills mb-3 deal-tags" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Search Tag</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Search Tag</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Search Tag</button>
-                </li>
-            </ul>
-            <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                <div class="row">
-                <?php
-                for ($p1 = 0; $p1 < 8; $p1++) {
-                    echo "<div class='card col-md-3'>
+
+                <ul class="nav nav-pills mb-3 deal-tags" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Search Tag</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Search Tag</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Search Tag</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                        <div class="row">
+                            <?php
+                            for ($p1 = 0; $p1 < 8; $p1++) {
+                                echo "<div class='card col-md-3'>
                     <img class='card-img-top' src='./assets/products.png' alt='Card image cap'>
                     <div class='card-body'>
                         <h5 class='card-title'>Product Name</h5>
@@ -298,16 +300,20 @@
                         <input type='button' class='btn primary' value='Add to Cart'>
                     </div>
                 </div>";
-                }
-                ?>
-
-            </div>
-                </div>
-                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                <div class="row">
-                <?php
-                for ($p1 = 0; $p1 < 8; $p1++) {
-                    echo "<div class='card col-md-3'>
+                            }
+                            ?>
+                            <div class="center-btn">
+                                <div class="explore-btn">
+                                    Explore More <img src="./assets/icons/more-arrow.svg" alt="" sizes="w-16" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                        <div class="row">
+                            <?php
+                            for ($p1 = 0; $p1 < 8; $p1++) {
+                                echo "<div class='card col-md-3'>
                     <img class='card-img-top' src='./assets/products.png' alt='Card image cap'>
                     <div class='card-body'>
                         <h5 class='card-title'>Product Name</h5>
@@ -316,16 +322,20 @@
                         <input type='button' class='btn primary' value='Add to Cart'>
                     </div>
                 </div>";
-                }
-                ?>
-
-            </div>
-                </div>
-                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-                <div class="row">
-                <?php
-                for ($p1 = 0; $p1 < 8; $p1++) {
-                    echo "<div class='card col-md-3'>
+                            }
+                            ?>
+                            <div class="center-btn">
+                                <div class="explore-btn">
+                                    Explore More <img src="./assets/icons/more-arrow.svg" alt="" sizes="w-16" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                        <div class="row">
+                            <?php
+                            for ($p1 = 0; $p1 < 8; $p1++) {
+                                echo "<div class='card col-md-3'>
                     <img class='card-img-top' src='./assets/products.png' alt='Card image cap'>
                     <div class='card-body'>
                         <h5 class='card-title'>Product Name</h5>
@@ -334,12 +344,17 @@
                         <input type='button' class='btn primary' value='Add to Cart'>
                     </div>
                 </div>";
-                }
-                ?>
+                            }
+                            ?>
 
-            </div>
+                            <div class="center-btn">
+                                <div class="explore-btn">
+                                    Explore More <img src="./assets/icons/more-arrow.svg" alt="" sizes="w-16" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
             </div>
 
@@ -439,8 +454,70 @@
         </div>
     </div>
 
+    <div class="section ad-banner">
+        <div class="container">
+            <div id="ad-banner-carousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#ad-banner-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#ad-banner-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#ad-banner-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./assets/hero-slides/slide 1.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/hero-slides/slide 2.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/hero-slides/slide 1.png" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#ad-banner-carousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#ad-banner-carousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="section cat-sec">
+        <div class="container">
+            <h2 class="section-header">Today Best Deals For You</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="cat-tile">
+                        <div class="cat-img">
+                            <img src="./assets/hero-slides/slide 1.png" alt="" srcset="">
+                        </div>
+                        <div class="cat-text">
+                            <h4 id="cat-name1">Category Name</h4>
+                            <input type="button" class="btn primary" value="Shop Now">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="cat-tile">
+                        <div class="cat-img">
+                            <img src="./assets/hero-slides/slide 1.png" alt="" srcset="">
+                        </div>
+                        <div class="cat-text">
+                            <h4 id="cat-name2">Category Name</h4>
+                            <input type="button" class="btn primary" value="Shop Now">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php include "./partials/home-footer.php"; ?>
     <script src="./js/bootstrap.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
---
+++ partials/home-footer.php
@@ -0,0 +1,62 @@
+<!-- <footer class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="col-2"></div>
+                <div class="col-2"></div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+</footer> -->
+
+<footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <img src="../assets/sanakin-logo.png"  alt="" srcset="">
+                    <!-- <h5 class="h1 text-white">FB.</h5> -->
+                    <p class="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                    <h5 class="text-white mb-3">Contact Us</h5>
+                    <div class="contact-details">
+                    <a class="small text-muted" href="#">123/S, Buddhaloka Mw<br> Suwarapola<br> Piliyandal</a>
+                    <a class="small text-muted" href="tel:+94763133646">(+94) 76 3133 646</a>
+                    </div>
+                    <p class="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="#">Sanakin.lk</a></p>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Pages</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Shop Online</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">What's New</a></li>
+                        <li><a href="#">Most Popular</a></li>
+                        <li><a href="#">Best Selling</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Categories</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="#">Category 1</a></li>
+                        <li><a href="#">Category 2</a></li>
+                        <li><a href="#">Category 3</a></li>
+                        <li><a href="#">Category 4</a></li>
+                        <li><a href="#">Category 5</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text-white mb-3">Newsletter</h5>
+                    <p class="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                    <form action="#">
+                        <div class="input-group mb-3">
+                            <input class="form-control" type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <button class="btn btn-primary" id="button-addon2" type="button"><i class="fas fa-paper-plane"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
