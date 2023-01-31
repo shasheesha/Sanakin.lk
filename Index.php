f5e6e8bc4a6e06877715caf442a0d3301ce35238
shasheeshajd <85898263+shasheesha@users.noreply.github.com>
2023-02-01 01:04:18 +0530

    nav-bar created
    build by shasheesha.
    the nav-bar is done. not linked. expect to do linking using JS.

--- Index.php
+++ Index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Sanakin.LK | Trusted Shops</title>
 </head>
+<?php include "./partials/home-navigation.php"; ?>
 <body>
     
+<?php include "./partials/home-footer.php"; ?>
 </body>
 </html>
