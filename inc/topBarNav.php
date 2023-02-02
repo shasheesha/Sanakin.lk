57ea9c934c20ef7084dcf1874a654961887f3ebf
Deshan Kariyawasam <kariyawasam.dk@gmail.com>
2023-02-02 23:24:47 +0530

    shop page

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -7,7 +7,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./">Home</a></li>
-                        <li class="nav-item"><a class="nav-link text-white" href="#">Shop Online</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="shop.php">Shop Online</a></li>
                         <li class="nav-item"><a class="nav-link text-white" href="#">What's New</a></li>
                         <li class="nav-item"><a class="nav-link text-white" href="#">Contact</a></li>
                         <li class="nav-item"><a class="nav-link text-white" href="#">About Us</a></li>
